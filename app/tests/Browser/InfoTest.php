dee9fe558a5fdb3fb6fd1540b5dafd0afde13db9
yagi1231 <at.sao.1231@gmail.com>
2022-03-24 20:27:11 +0900

    viewテスト

--- app/tests/Browser/InfoTest.php
+++ app/tests/Browser/InfoTest.php
@@ -20,22 +20,25 @@ class InfoTest extends DuskTestCase
        parent::setUp();
        // テストユーザ作成
        $user = factory(User::class)->create();  
-       $response = $this->actingAs($user);
     }
 
     public function test_顧客新規登録()
     {
-        $this->browse(function (Browser $browser) {
-            $browser->loginAs(User::find(1))->visit('/infos/create')
-                    ->clickLink('新規登録') // 一覧画面に遷移 // 一覧画面で新規作成リンクをクリック
+        $user = factory(User::class)->create();
+
+        $this->browse(function (Browser $browser) use ($user) {
+            $browser->loginAs($user)
+                    ->visit('/infos/create')
+                    ->acceptDialog()// 一覧画面に遷移 // 一覧画面で新規作成リンクをクリック
                     ->type('input[name="name"]', 'test_name') // タイトルを入力する
                     ->type('input[name="address"]', 'aaa@gamil.com') 
                     ->type('input[name="telnum"]', '08012345786')
                     ->type('input[name="remarks"]', 'なし') // 著者を入力する
                     ->press('情報を登録する')
                     ->acceptDialog() // 送信ボタンをクリック
-                    ->assertPathIs('/infos/'.$browser->id) // 一覧画面に遷移を確認
-                    ->assertSee('顧客一覧表'); // 「タイトルテスト」というテキストが含まれていること
+                    ->assertPathIs('/infos/'.$browser->id)
+                    ->acceptDialog()
+                    ; // 「タイトルテスト」というテキストが含まれていること
         });
     }
 }
