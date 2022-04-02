8f4fbb704f853bfbc99fc2a734aa9744dbdb2462
yagi1231 <at.sao.1231@gmail.com>
2022-04-03 07:47:50 +0900

    duskテスト

--- app/tests/Browser/InfoTest.php
+++ app/tests/Browser/InfoTest.php
@@ -25,120 +25,120 @@ class InfoTest extends DuskTestCase
        $user = factory(User::class)->create();  
     }
 
-    // public function test_顧客新規登録()
-    // {
-    //     $this->browse(function (Browser $browser) {
-    //         $browser->loginAs(User::find(1))
-    //                 ->visit('/infos/create')
-    //                 ->type('input[name="name"]', 'test_name')
-    //                 ->type('input[name="address"]', 'aaa@gamil.com') 
-    //                 ->type('input[name="telnum"]', '08012345786')
-    //                 ->type('input[name="remarks"]', 'なし')
-    //                 ->press('情報を登録する')
-    //                 ->acceptDialog()
-    //                 ->assertSee('作成完了')
-    //                 ->pause(2000);
-    //     });
-    // }
+    public function test_顧客新規登録()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->loginAs(User::find(1))
+                    ->visit('/infos/create')
+                    ->type('input[name="name"]', 'test_name')
+                    ->type('input[name="address"]', 'aaa@gamil.com') 
+                    ->type('input[name="telnum"]', '08012345786')
+                    ->type('input[name="remarks"]', 'なし')
+                    ->press('情報を登録する')
+                    ->acceptDialog()
+                    ->assertSee('作成完了')
+                    ->pause(2000);
+        });
+    }
 
-    // public function test_顧客詳細_顧客編集()
-    // {
-    //     $this->browse(function (Browser $browser){
-    //         $browser->loginAs(User::find(1))
-    //                 ->visit('/infos/index')
-    //                 ->clicklink('test様')
-    //                 ->assertInputValue('input[name="name"]', 'test')
-    //                 ->assertInputValue('input[name="address"]', '埼玉') 
-    //                 ->assertInputValue('input[name="telnum"]', '08087654321')
-    //                 ->assertInputValue('input[name="remarks"]', 'なし')
-    //                 ->clicklink('情報を編集する')
-    //                 ->type('input[name="name"]', 'test_name') 
-    //                 ->type('input[name="address"]', 'aaa@gamil.com') 
-    //                 ->type('input[name="telnum"]', '08012345786')
-    //                 ->type('input[name="remarks"]', 'なし')
-    //                 ->click('@edit-btn')
-    //                 ->acceptDialog()
-    //                 ->pause(2000)
-    //                 ->assertSee('更新完了');
-    //     });
-    // }
+    public function test_顧客詳細_顧客編集()
+    {
+        $this->browse(function (Browser $browser){
+            $browser->loginAs(User::find(1))
+                    ->visit('/infos/index')
+                    ->clicklink('test様')
+                    ->assertInputValue('input[name="name"]', 'test')
+                    ->assertInputValue('input[name="address"]', '埼玉') 
+                    ->assertInputValue('input[name="telnum"]', '08087654321')
+                    ->assertInputValue('input[name="remarks"]', 'なし')
+                    ->clicklink('情報を編集する')
+                    ->type('input[name="name"]', 'test_name') 
+                    ->type('input[name="address"]', 'aaa@gamil.com') 
+                    ->type('input[name="telnum"]', '08012345786')
+                    ->type('input[name="remarks"]', 'なし')
+                    ->click('@edit-btn')
+                    ->acceptDialog()
+                    ->pause(2000)
+                    ->assertSee('更新完了');
+        });
+    }
     
-    // public function test_顧客削除()
-    // {
-    //     $this->browse(function (Browser $browser) {
-    //         $browser->loginAs(User::find(1))
-    //                 ->visit('/infos/index')
-    //                 ->press('@delete-btn')
-    //                 ->acceptDialog()
-    //                 ->assertSee('削除完了')
-    //                 ->pause(2000);
-    //     });
-    // }
+    public function test_顧客削除()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->loginAs(User::find(1))
+                    ->visit('/infos/index')
+                    ->press('@delete-btn')
+                    ->acceptDialog()
+                    ->assertSee('削除完了')
+                    ->pause(2000);
+        });
+    }
 
-    // public function test_顧客から予約に情報を渡す()
-    // {
-    //     $this->browse(function (Browser $browser){
-    //         $browser->loginAs(User::find(1))
-    //                 ->visit('/infos/index')
-    //                 ->clicklink('test様')
-    //                 ->click('@new-btn')
-    //                 ->assertInputValue('input[name="name"]', 'test')
-    //                 ->assertInputValue('input[name="address"]', '埼玉') 
-    //                 ->assertInputValue('input[name="telnum"]', '08087654321')
-    //                 ->assertInputValue('input[name="remarks"]', 'なし')
-    //                 ->type('textarea[name="order"]', 'ステーキ') 
-    //                 ->type('input[name="time"]', '03-22-2022') 
-    //                 ->type('input[name="backtime"]', '11:00')
-    //                 ->type('input[name="sumprice"]', '780')
-    //                 ->type('input[name="category"]', 'KOUCH') 
-    //                 ->type('input[name="categoryname"]', '企業')
-    //                 ->click('@new-submit')
-    //                 ->acceptDialog()
-    //                 ->pause(2000)
-    //                 ->acceptDialog()
-    //                 ->assertSee('作成完了');
+    public function test_顧客から予約に情報を渡す()
+    {
+        $this->browse(function (Browser $browser){
+            $browser->loginAs(User::find(1))
+                    ->visit('/infos/index')
+                    ->clicklink('test様')
+                    ->click('@new-btn')
+                    ->assertInputValue('input[name="name"]', 'test')
+                    ->assertInputValue('input[name="address"]', '埼玉') 
+                    ->assertInputValue('input[name="telnum"]', '08087654321')
+                    ->assertInputValue('input[name="remarks"]', 'なし')
+                    ->type('textarea[name="order"]', 'ステーキ') 
+                    ->type('input[name="time"]', '03-22-2022') 
+                    ->type('input[name="backtime"]', '11:00')
+                    ->type('input[name="sumprice"]', '780')
+                    ->type('input[name="category"]', 'KOUCH') 
+                    ->type('input[name="categoryname"]', '企業')
+                    ->click('@new-submit')
+                    ->acceptDialog()
+                    ->pause(2000)
+                    ->acceptDialog()
+                    ->assertSee('作成完了');
 
-    //     });
-    // }
+        });
+    }
 
-    // public function test_顧客検索_キーワード()
-    // {
-    //     $this->browse(function (Browser $browser){
-    //         $browser->loginAs(User::find(1))
-    //                 ->visit('/infos/index')
-    //                 ->type('input[name="search"]', 'test2')
-    //                 ->press('@search')
-    //                 ->assertSee('test2様')
-    //                 ->assertDontSee('test3様')
-    //                 ->type('input[name="search"]', 'test3')
-    //                 ->press('@search')
-    //                 ->assertSee('test3様')
-    //                 ->assertDontSee('test2様');
-    //     });
-    // }
+    public function test_顧客検索_キーワード()
+    {
+        $this->browse(function (Browser $browser){
+            $browser->loginAs(User::find(1))
+                    ->visit('/infos/index')
+                    ->type('input[name="search"]', 'test2')
+                    ->press('@search')
+                    ->assertSee('test2様')
+                    ->assertDontSee('test3様')
+                    ->type('input[name="search"]', 'test3')
+                    ->press('@search')
+                    ->assertSee('test3様')
+                    ->assertDontSee('test2様');
+        });
+    }
 
-    // public function test_ペーネーション5以上()
-    // {
-    //     $this->browse(function (Browser $browser){
-    //         $browser->loginAs(User::find(1))
-    //                 ->visit('/infos/index')
-    //                 ->click('a.page-link')
-    //                 ->assertSee('test6様')
-    //                 ->assertDontSee('test様')
-    //                 ->click('a.page-link')
-    //                 ->assertSee('test様')
-    //                 ->assertDontSee('test6様');
-    //     });
-    // }
+    public function test_ペーネーション5以上()
+    {
+        $this->browse(function (Browser $browser){
+            $browser->loginAs(User::find(1))
+                    ->visit('/infos/index')
+                    ->click('a.page-link')
+                    ->assertSee('test6様')
+                    ->assertDontSee('test様')
+                    ->click('a.page-link')
+                    ->assertSee('test様')
+                    ->assertDontSee('test6様');
+        });
+    }
 
-    // public function test_ペーネーション5以下の場合()
-    // {
-    //     $this->browse(function (Browser $browser){
-    //         $browser->loginAs(User::find(1))
-    //                 ->visit('/infos/index')
-    //                 ->press('@delete-btn')
-    //                 ->acceptDialog()
-    //                 ->assertSourceMissing('pagination.m-0');
-    //     });
-    // }
+    public function test_ペーネーション5以下の場合()
+    {
+        $this->browse(function (Browser $browser){
+            $browser->loginAs(User::find(1))
+                    ->visit('/infos/index')
+                    ->press('@delete-btn')
+                    ->acceptDialog()
+                    ->assertSourceMissing('pagination.m-0');
+        });
+    }
 }
--- app/tests/Browser/ReservationTest.php
+++ app/tests/Browser/ReservationTest.php
@@ -25,133 +25,133 @@ class ReservationTest extends DuskTestCase
        $user = factory(User::class)->create();  
     }
 
-    // public function test_予約詳細()
-    // {
-    //     $this->browse(function (Browser $browser) {
-    //         $browser->loginAs(User::find(1))
-    //                 ->visit('/reservations/index')
-    //                 ->acceptDialog()
-    //                 ->clicklink('テスト様')
-    //                 ->assertSee('テスト様')
-    //                 ->assertSee('埼玉') 
-    //                 ->assertSee('07087976545')
-    //                 ->assertSee('2022-03-21')
-    //                 ->assertSee('13:00-13:30') 
-    //                 ->assertSee('KOUCH')
-    //                 ->assertSee('企業')
-    //                 ->assertSee('ステーキ　生姜焼き 肉じゃが') 
-    //                 ->assertSee('07087976545')
-    //                 ->assertSee('2040')
-    //                 ->assertSee('特になし')
-    //                 ->pause(2000);
-    //     });
-    // }
+    public function test_予約詳細()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->loginAs(User::find(1))
+                    ->visit('/reservations/index')
+                    ->acceptDialog()
+                    ->clicklink('テスト様')
+                    ->assertSee('テスト様')
+                    ->assertSee('埼玉') 
+                    ->assertSee('07087976545')
+                    ->assertSee('2022-03-21')
+                    ->assertSee('13:00-13:30') 
+                    ->assertSee('KOUCH')
+                    ->assertSee('企業')
+                    ->assertSee('ステーキ　生姜焼き 肉じゃが') 
+                    ->assertSee('07087976545')
+                    ->assertSee('2040')
+                    ->assertSee('特になし')
+                    ->pause(2000);
+        });
+    }
 
-    // public function test_予約編集()
-    // {
-    //     $this->browse(function (Browser $browser){
-    //         $browser->loginAs(User::find(1))
-    //                 ->visit('/reservations/index')
-    //                 ->acceptDialog()
-    //                 ->clicklink('テスト様')
-    //                 ->press('@edit-btn')
-    //                 ->type('input[name="name"]', 'テスト8') 
-    //                 ->type('input[name="address"]', '福島') 
-    //                 ->type('input[name="telnum"]', '08012340986')
-    //                 ->type('textarea[name="order"]', 'ステーキ') 
-    //                 ->type('input[name="sumprice"]', '680') 
-    //                 ->type('input[name="time"]', '2022-08-03')
-    //                 ->type('input[name="backtime"]', '14:00')
-    //                 ->type('input[name="category"]', 'コラボ') 
-    //                 ->type('input[name="categoryname"]', '民家') 
-    //                 ->type('input[name="remarks"]', 'なし')
-    //                 ->press('@edit-create-btn')
-    //                 ->acceptDialog()
-    //                 ->acceptDialog()
-    //                 ->pause(2000)
-    //                 ->assertSee('更新完了');
-    //     });
-    // }
+    public function test_予約編集()
+    {
+        $this->browse(function (Browser $browser){
+            $browser->loginAs(User::find(1))
+                    ->visit('/reservations/index')
+                    ->acceptDialog()
+                    ->clicklink('テスト様')
+                    ->press('@edit-btn')
+                    ->type('input[name="name"]', 'テスト8') 
+                    ->type('input[name="address"]', '福島') 
+                    ->type('input[name="telnum"]', '08012340986')
+                    ->type('textarea[name="order"]', 'ステーキ') 
+                    ->type('input[name="sumprice"]', '680') 
+                    ->type('input[name="time"]', '2022-08-03')
+                    ->type('input[name="backtime"]', '14:00')
+                    ->type('input[name="category"]', 'コラボ') 
+                    ->type('input[name="categoryname"]', '民家') 
+                    ->type('input[name="remarks"]', 'なし')
+                    ->press('@edit-create-btn')
+                    ->acceptDialog()
+                    ->acceptDialog()
+                    ->pause(2000)
+                    ->assertSee('更新完了');
+        });
+    }
     
-    // public function test_顧客削除()
-    // {
-    //     $this->browse(function (Browser $browser) {
-    //         $browser->loginAs(User::find(1))
-    //                 ->visit('/reservations/index')
-    //                 ->acceptDialog()
-    //                 ->clicklink('テスト様')
-    //                 ->press('@delete-btn')
-    //                 ->acceptDialog()
-    //                 ->acceptDialog()
-    //                 ->assertSee('削除完了')
-    //                 ->pause(2000);
-    //     });
-    // }
+    public function test_顧客削除()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->loginAs(User::find(1))
+                    ->visit('/reservations/index')
+                    ->acceptDialog()
+                    ->clicklink('テスト様')
+                    ->press('@delete-btn')
+                    ->acceptDialog()
+                    ->acceptDialog()
+                    ->assertSee('削除完了')
+                    ->pause(2000);
+        });
+    }
 
-    // public function test_顧客検索_キーワード_日付()
-    // {
-    //     $this->browse(function (Browser $browser){
-    //         $browser->loginAs(User::find(1))
-    //                 ->visit('/reservations/index')
-    //                 ->acceptDialog()
-    //                 ->type('input[name="search"]', ' テスト2')
-    //                 ->type('input[name="date-search"]', '07-22-2022')
-    //                 ->press('@search')
-    //                 ->acceptDialog()
-    //                 ->assertSee('テスト2様')
-    //                 ->assertDontSee('テスト様');
-    //     });
-    // }
+    public function test_顧客検索_キーワード_日付()
+    {
+        $this->browse(function (Browser $browser){
+            $browser->loginAs(User::find(1))
+                    ->visit('/reservations/index')
+                    ->acceptDialog()
+                    ->type('input[name="search"]', ' テスト2')
+                    ->type('input[name="date-search"]', '07-22-2022')
+                    ->press('@search')
+                    ->acceptDialog()
+                    ->assertSee('テスト2様')
+                    ->assertDontSee('テスト様');
+        });
+    }
 
-    // public function test_顧客検索_キーワード()
-    // {
-    //     $this->browse(function (Browser $browser){
-    //         $browser->loginAs(User::find(1))
-    //                 ->visit('/reservations/index')
-    //                 ->acceptDialog()
-    //                 ->type('input[name="search"]', 'テスト2')
-    //                 ->press('@search')
-    //                 ->acceptDialog()
-    //                 ->assertSee('テスト2様')
-    //                 ->assertDontSee('test3様')
-    //                 ->type('input[name="search"]', 'テスト3')
-    //                 ->press('@search')
-    //                 ->acceptDialog()
-    //                 ->assertSee('テスト3様')
-    //                 ->assertDontSee('テスト2様');
-    //     });
-    // }
+    public function test_顧客検索_キーワード()
+    {
+        $this->browse(function (Browser $browser){
+            $browser->loginAs(User::find(1))
+                    ->visit('/reservations/index')
+                    ->acceptDialog()
+                    ->type('input[name="search"]', 'テスト2')
+                    ->press('@search')
+                    ->acceptDialog()
+                    ->assertSee('テスト2様')
+                    ->assertDontSee('test3様')
+                    ->type('input[name="search"]', 'テスト3')
+                    ->press('@search')
+                    ->acceptDialog()
+                    ->assertSee('テスト3様')
+                    ->assertDontSee('テスト2様');
+        });
+    }
 
-    // public function test_顧客検索_日付()
-    // {
-    //     $this->browse(function (Browser $browser){
-    //         $browser->loginAs(User::find(1))
-    //                 ->visit('/reservations/index')
-    //                 ->acceptDialog()
-    //                 ->type('input[name="date-search"]', '06-23-2022')
-    //                 ->press('@search')
-    //                 ->acceptDialog()
-    //                 ->assertSee('テスト3様')
-    //                 ->assertDontSee('テスト様');
-    //     });
-    // }
+    public function test_顧客検索_日付()
+    {
+        $this->browse(function (Browser $browser){
+            $browser->loginAs(User::find(1))
+                    ->visit('/reservations/index')
+                    ->acceptDialog()
+                    ->type('input[name="date-search"]', '06-23-2022')
+                    ->press('@search')
+                    ->acceptDialog()
+                    ->assertSee('テスト3様')
+                    ->assertDontSee('テスト様');
+        });
+    }
 
-    // public function test_ペーネーション5以上()
-    // {
-    //     $this->browse(function (Browser $browser){
-    //         $browser->loginAs(User::find(1))
-    //                 ->visit('/reservations/index')
-    //                 ->acceptDialog()
-    //                 ->click('a.page-link')
-    //                 ->acceptDialog()
-    //                 ->assertSee('テスト7様')
-    //                 ->assertDontSee('テスト様')
-    //                 ->click('a.page-link')
-    //                 ->acceptDialog()
-    //                 ->assertSee('テスト様')
-    //                 ->assertDontSee('テスト7様');
-    //     });
-    // }
+    public function test_ペーネーション5以上()
+    {
+        $this->browse(function (Browser $browser){
+            $browser->loginAs(User::find(1))
+                    ->visit('/reservations/index')
+                    ->acceptDialog()
+                    ->click('a.page-link')
+                    ->acceptDialog()
+                    ->assertSee('テスト7様')
+                    ->assertDontSee('テスト様')
+                    ->click('a.page-link')
+                    ->acceptDialog()
+                    ->assertSee('テスト様')
+                    ->assertDontSee('テスト7様');
+        });
+    }
 
     public function test_ペーネーション5以下の場合()
     {
@@ -191,72 +191,72 @@ class ReservationTest extends DuskTestCase
         });
     }
 
-    // public function test_売上ページ変更確認()
-    // {
-    //     $this->browse(function (Browser $browser){
-    //         $browser->loginAs(User::find(1))
-    //                 ->visit('/reservations/sales/sum_sale')
-    //                 ->clicklink('日別平均')
-    //                 ->assertPathIs('/reservations/sales/ave_sale')
+    public function test_売上ページ変更確認()
+    {
+        $this->browse(function (Browser $browser){
+            $browser->loginAs(User::find(1))
+                    ->visit('/reservations/sales/sum_sale')
+                    ->clicklink('日別平均')
+                    ->assertPathIs('/reservations/sales/ave_sale')
 
-    //                 ->clicklink('月別売上')
-    //                 ->assertPathIs('/reservations/sales/month_sum_sale')
+                    ->clicklink('月別売上')
+                    ->assertPathIs('/reservations/sales/month_sum_sale')
 
-    //                 ->clicklink('月別平均')
-    //                 ->assertPathIs('/reservations/sales/month_ave_sale')
+                    ->clicklink('月別平均')
+                    ->assertPathIs('/reservations/sales/month_ave_sale')
 
-    //                 ->clicklink('日別平均')
-    //                 ->assertPathIs('/reservations/sales/sum_sale');
-    //     });
-    // }
+                    ->clicklink('日別平均')
+                    ->assertPathIs('/reservations/sales/sum_sale');
+        });
+    }
 
-    // public function test_売上ページ検索確認()
-    // {
-    //     $this->browse(function (Browser $browser){
-    //         $browser->loginAs(User::find(1))
-    //                 ->visit('/reservations/sales/ave_sale')
-    //                 ->type('input[name="search"]', '03-21-2022')
-    //                 ->press('@search')
-    //                 ->assertSee('2022/03/21')
-    //                 ->assertSee('2件')
-    //                 ->assertSee('￥1,700')
+    public function test_売上ページ検索確認()
+    {
+        $this->browse(function (Browser $browser){
+            $browser->loginAs(User::find(1))
+                    ->visit('/reservations/sales/ave_sale')
+                    ->type('input[name="search"]', '03-21-2022')
+                    ->press('@search')
+                    ->assertSee('2022/03/21')
+                    ->assertSee('2件')
+                    ->assertSee('￥1,700')
                
-    //                 ->visit('/reservations/sales/month_sum_sale')
-    //                 ->press('@search')
-    //                 ->assertSee('2022/04')
-    //                 ->assertSee('1件')
-    //                 ->assertSee('￥680')
+                    ->visit('/reservations/sales/month_sum_sale')
+                    ->press('@search')
+                    ->assertSee('2022/04')
+                    ->assertSee('1件')
+                    ->assertSee('￥680')
 
 
-    //                 ->visit('/reservations/sales/month_ave_sale')
-    //                 ->press('@search')
-    //                 ->assertSee('2022/04')
-    //                 ->assertSee('1件')
-    //                 ->assertSee('￥680')
+                    ->visit('/reservations/sales/month_ave_sale')
+                    ->press('@search')
+                    ->assertSee('2022/04')
+                    ->assertSee('1件')
+                    ->assertSee('￥680')
 
-    //                 ->visit('/reservations/sales/sum_sale')
-    //                 ->type('input[name="search"]', '03-21-2022')
-    //                 ->assertSee('2022/03/21')
-    //                 ->assertSee('2件')
-    //                 ->assertSee('￥3,400');
-    //     });
-    // }
+                    ->visit('/reservations/sales/sum_sale')
+                    ->type('input[name="search"]', '03-21-2022')
+                    ->assertSee('2022/03/21')
+                    ->assertSee('2件')
+                    ->assertSee('￥3,400');
+        });
+    }
 
-    // public function test_売上ページ_5つ以上でページネーション確認()
-    // {
-    //     $this->browse(function (Browser $browser){
-    //         $browser->loginAs(User::find(1))
-    //                 ->visit('/reservations/sales/ave_sale')
-    //                 ->assertSourceHas('d-flex justify-content-center mt-5 mb-5')
+    public function test_売上ページ_5つ以上でページネーション確認()
+    {
+        $this->browse(function (Browser $browser){
+            $browser->loginAs(User::find(1))
+                    ->visit('/reservations/sales/ave_sale')
+                    ->assertSourceHas('d-flex justify-content-center mt-5 mb-5')
                
-    //                 ->visit('/reservations/sales/month_sum_sale')
-    //                 ->assertSourceHas('d-flex justify-content-center mt-5 mb-5')
+                    ->visit('/reservations/sales/month_sum_sale')
+                    ->assertSourceHas('d-flex justify-content-center mt-5 mb-5')
 
-    //                 ->visit('/reservations/sales/month_ave_sale')
-    //                 ->assertSourceHas('d-flex justify-content-center mt-5 mb-5')
+                    ->visit('/reservations/sales/month_ave_sale')
+                    ->assertSourceHas('d-flex justify-content-center mt-5 mb-5')
 
-    //                 ->visit('/reservations/sales/sum_sale')
-    //                 ->assertSourceHas('d-flex justify-content-center mt-5 mb-5');
-    //     });
-    // }
+                    ->visit('/reservations/sales/sum_sale')
+                    ->assertSourceHas('d-flex justify-content-center mt-5 mb-5');
+        });
+    }
 }
