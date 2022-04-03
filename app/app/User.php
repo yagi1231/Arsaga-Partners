4e718b3b57fe4a20dce437e4f990da0a84a467f0
yagi1231 <at.sao.1231@gmail.com>
2022-04-03 17:39:38 +0900

    circleci

--- app/app/User.php
+++ app/app/User.php
@@ -15,6 +15,8 @@ class User extends Authenticatable
      *
      * @var array
      */
+
+    protected $table = "users";
     protected $fillable = [
         'name', 'email', 'password',
     ];
@@ -36,6 +38,4 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-
-    protected $table = "Users";
 }
--- app/config/database.php
+++ app/config/database.php
@@ -39,7 +39,7 @@ return [
             'driver' => 'mysql',
             'host' => '127.0.0.1',
             'port' => '3306',
-            'database' => 'circle_testing',
+            'database' => 'circle_test',
             'username' => 'root',
             'password' => '',
             'charset' => 'utf8mb4',
