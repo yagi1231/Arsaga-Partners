3597641bfb1d7876fb10be6a6a92ee2194570859
yagi1231 <at.sao.1231@gmail.com>
2022-04-04 12:39:19 +0900

    circleci

--- app/config/database.php
+++ app/config/database.php
@@ -39,7 +39,7 @@ return [
             'driver' => 'mysql',
             'host' => '127.0.0.1',
             'port' => '3306',
-            'database' => 'circle_test',
+            'database' => 'port',
             'username' => 'root',
             'password' => 'password',
             'charset' => 'utf8mb4',
